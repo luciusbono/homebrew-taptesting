031ea13fda35d90b1018946ab7f37760fb39223d
Lucius Bono <luciusbono@gmail.com>
2017-10-12 16:28:51 -0700

    Swapping this out for a Crashplan Pro E install

--- Casks/crashplan-proe.rb
+++ Casks/crashplan-proe.rb
@@ -2,7 +2,7 @@ cask 'crashplan' do
   version '4.5.2'
   sha256 '855e63d87a156754e0e03f2cd274d204a36682fb541809aa1400613e367234a0'
 
-  url "https://download.crashplan.com/installs/mac/install/CrashPlan/CrashPlan_#{version}_Mac.dmg"
+  url "https://download.code42.com/installs/mac/install/CrashPlanPROe/CrashPlanPROe_#{version}_Mac.dmg"
   name 'CrashPlan'
   homepage 'https://www.crashplan.com/'
 
