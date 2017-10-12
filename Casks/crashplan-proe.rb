5413ea620d0725595bfaf3cc00f060583598248f
Lucius Bono <luciusbono@gmail.com>
2017-10-12 16:36:29 -0700

    Changing the name identifier in the ruby file.

--- Casks/crashplan-proe.rb
+++ Casks/crashplan-proe.rb
@@ -1,9 +1,9 @@
-cask 'crashplan' do
+cask 'crashplan-proe' do
   version '4.5.2'
   sha256 '855e63d87a156754e0e03f2cd274d204a36682fb541809aa1400613e367234a0'
 
   url "https://download.code42.com/installs/mac/install/CrashPlanPROe/CrashPlanPROe_#{version}_Mac.dmg"
-  name 'CrashPlan'
+  name 'CrashPlan ProE'
   homepage 'https://www.crashplan.com/'
 
   auto_updates true
